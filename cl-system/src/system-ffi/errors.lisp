c85029685f87fa34ff224a8884bb64f0359273bc
Nicolas Martyanoff <nicolas@n16f.net>
2022-12-08 17:01:16 +0100

    system: add retry support to the SYSTEM-FUNCALL macro

--- cl-system/src/system-ffi/errors.lisp
+++ cl-system/src/system-ffi/errors.lisp
@@ -33,22 +33,40 @@
       (ffi:decode-foreign-string ptr))))
 
 (defmacro system-funcall ((name signature &rest args)
-                          &key (errorp 'minus1p))
-  "Call a foreign function. If ERRORP is not null, use it to determine if the
-return value of the call indicates an error. When this is the case, signal a
-SYSTEM-ERROR condition containing information about the error."
+                          &key (errorp #'minus1p)
+                               (retryp #'eintrp))
+  "Call a foreign function.
+
+If ERRORP is not null, use it to determine if the return value of the call
+indicates an error. When this is the case, signal a SYSTEM-ERROR condition
+containing information about the error.
+
+If RETRYP is not null, use it to determine if the value of errno indicates
+that the call should be executed again. If this is the case, execute the call
+again with the same arguments."
   (let ((errorp-var (gensym "ERRORP-VAR-"))
+        (retryp-var (gensym "RETRYP-VAR-"))
         (value (gensym "VALUE-"))
         (errno (gensym "ERRNO-")))
     `(let ((,errorp-var ,errorp)
-           (,value (ffi:foreign-funcall ,name ,signature ,@args)))
-       (when (and ,errorp-var (funcall ,errorp-var ,value))
-         (let ((,errno (ffi:errno)))
-           (error 'system-error :function ,name :value ,errno
-                                :description (strerror-l ,errno))))
-       ,value)))
+           (,retryp-var ,retryp))
+       (block body
+         (tagbody retry
+            (let ((,value (ffi:foreign-funcall ,name ,signature ,@args)))
+              (when (and ,errorp-var (funcall ,errorp-var ,value))
+                (let ((,errno (ffi:errno)))
+                  (if (and ,retryp-var (funcall ,retryp-var ,errno))
+                      (go retry)
+                      (error 'system-error :function ,name :value ,errno
+                                           :description (strerror-l ,errno)))))
+              (return-from body ,value)))))))
 
 (declaim (inline minus1p))
 (defun minus1p (n)
   (declare (integer n))
   (= n -1))
+
+(declaim (inline eintrp))
+(defun eintrp (value)
+  (declare (type integer value))
+  (= value #.(ffi:encode-foreign-value :eintr 'errno)))
