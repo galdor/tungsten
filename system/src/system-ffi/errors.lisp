9737264c385e5404e9f866da2f739d8eaa273285
Nicolas Martyanoff <nicolas@n16f.net>
2022-12-12 17:44:02 +0100

    system: improve SYSTEM-FUNCALL

--- system/src/system-ffi/errors.lisp
+++ system/src/system-ffi/errors.lisp
@@ -17,6 +17,10 @@
        (format stream "System function ~S failed~@[ with error ~A~]~@[: ~A~]."
                function value description)))))
 
+(defun errno-value (return-value)
+  (declare (ignore return-value))
+  (ffi:errno))
+
 ;; POSIX defines three functions that return a description of an errno value:
 ;; strerror(), strerror_r(), and strerror_l(). Infortunately strerror() is not
 ;; thread safe, and strerror_r() has two versions (GNU and POSIX) that cannot
@@ -33,13 +37,24 @@
       (ffi:decode-foreign-string ptr))))
 
 (defmacro system-funcall ((name signature &rest args)
-                          &key (errorp #'minus1p)
-                               (retryp #'eintrp))
+                          &key (errorp
+                                (case (car (last signature))
+                                  (:int ''minus1p)
+                                  (:pointer ''ffi:null-pointer-p)))
+                               (error-value-function ''errno-value)
+                               (error-description-function ''strerror-l)
+                               (retryp ''eintrp))
   "Call a foreign function.
 
-If ERRORP is not null, use it to determine if the return value of the call
+If ERRORP is not null, it is used to determine if the return value of the call
 indicates an error. When this is the case, signal a SYSTEM-ERROR condition
 containing information about the error.
+
+If ERROR-VALUE-FUNCTION is not null, it is used to obtain a value representing
+the error.
+
+If ERROR-DESCRIPTION-FUNCTION is not null, it is used to obtain a textual
+description of the error value.
 
 If RETRYP is not null, use it to determine if the value of errno indicates
 that the call should be executed again. If this is the case, execute the call
@@ -47,18 +62,21 @@ again with the same arguments."
   (let ((errorp-var (gensym "ERRORP-VAR-"))
         (retryp-var (gensym "RETRYP-VAR-"))
         (value (gensym "VALUE-"))
-        (errno (gensym "ERRNO-")))
+        (error-value (gensym "ERROR-VALUE-")))
     `(let ((,errorp-var ,errorp)
            (,retryp-var ,retryp))
        (block body
          (tagbody retry
             (let ((,value (ffi:foreign-funcall ,name ,signature ,@args)))
               (when (and ,errorp-var (funcall ,errorp-var ,value))
-                (let ((,errno (ffi:errno)))
-                  (if (and ,retryp-var (funcall ,retryp-var ,errno))
+                (let ((,error-value (funcall ,error-value-function ,value)))
+                  (if (and ,retryp-var (funcall ,retryp-var ,error-value))
                       (go retry)
-                      (error 'system-error :function ,name :value ,errno
-                                           :description (strerror-l ,errno)))))
+                      (error 'system-error
+                             :function ,name
+                             :value ,error-value
+                             :description (funcall ,error-description-function
+                                                   ,error-value)))))
               (return-from body ,value)))))))
 
 (declaim (inline minus1p))
