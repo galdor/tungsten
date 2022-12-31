d11b0f01233a1cfb29bbaee3f1ef869f4e5289f0
Nicolas Martyanoff <nicolas@n16f.net>
2022-12-31 20:41:58 +0100

    system: call shutdown() before closing tcp stream sockets

--- system/src/tcp-stream.lisp
+++ system/src/tcp-stream.lisp
@@ -3,6 +3,12 @@
 (defclass tcp-stream (network-stream)
   ())
 
+(defmethod close :before ((stream tcp-stream) &key abort)
+  (declare (ignore abort))
+  (with-slots (socket) stream
+    (ignore-errors
+     (shutdown socket :shut-rdwr))))
+
 (defmethod read-network-stream ((stream tcp-stream) octets start end)
   (declare (type core:octet-vector octets)
            (type (integer 0) start end))
