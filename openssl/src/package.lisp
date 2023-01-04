31638f36d547bdb14ee09b8be776679962f53955
Nicolas Martyanoff <nicolas@n16f.net>
2023-01-04 16:10:00 +0100

    openssl: export error types and accessors

--- openssl/src/package.lisp
+++ openssl/src/package.lisp
@@ -2,6 +2,18 @@
   (:use :cl)
   (:export
    #:library-version
+   #:openssl-error
+   #:openssl-error-value
+   #:openssl-error-reason
+   #:openssl-error-description
+   #:openssl-error-file
+   #:openssl-error-line
+   #:openssl-error-function
+   #:openssl-error-data
+   #:openssl-error-flags
+   #:openssl-error-stack
+   #:openssl-error-stack-function
+   #:openssl-error-stack-errors
 
    #:*default-ca-certificate-directory-paths*
    #:tls-client
