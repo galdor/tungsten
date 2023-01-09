4e60f57633dbd940ce4a03cfbfa43155834db99a
Nicolas Martyanoff <nicolas@n16f.net>
2023-01-09 10:25:10 +0100

    system: remove inline declarations

--- system/src/concurrency.lisp
+++ system/src/concurrency.lisp
@@ -7,16 +7,13 @@
 (deftype mutex ()
   '%mutex)
 
-(declaim (inline make-mutex))
 (defun make-mutex (&key name)
   (declare (type (or string null) name))
   (%make-mutex :name name))
 
-(declaim (inline acquire-mutex))
 (defun acquire-mutex (mutex &key (wait t))
   (%acquire-mutex mutex :wait wait))
 
-(declaim (inline release-mutex))
 (defun release-mutex (mutex)
   (%release-mutex mutex))
 
