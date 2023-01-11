8183e64b3405d07f20428823ea6500a95fc51433
Nicolas Martyanoff <nicolas@n16f.net>
2023-01-11 18:50:32 +0100

    http: add connection handling threads

--- http/src/server.lisp
+++ http/src/server.lisp
@@ -7,18 +7,39 @@
     :reader server-mutex)
    (tcp-server
     :type (or system:tcp-server null)
-    :initform nil)))
+    :initform nil
+    :accessor server-tcp-server)
+   (connection-handlers
+    :type list
+    :initform nil
+    :accessor server-connection-handlers)))
 
-(defun start-server (host port)
-  (let ((server (make-instance 'server)))
-    (setf (slot-value server 'tcp-server)
-          (system:start-tcp-server host port
-                                   (lambda (stream) (close stream))))
-    server))
+(defun start-server (host port &key (nb-threads 1))
+  (let ((server (make-instance 'server))
+        (connection-handlers nil))
+    (core:abort-protect
+        (progn
+          (dotimes (i nb-threads)
+            (push (system:make-thread "http-connection-handler"
+                                      (lambda ()
+                                        ;; TODO
+                                        nil))
+                  connection-handlers))
+          (setf (server-connection-handlers server) connection-handlers)
+          (setf (server-tcp-server server)
+                (system:start-tcp-server
+                 host port
+                 (lambda (stream)
+                   ;; TODO
+                   (close stream))))
+          server)
+      (mapc 'system:join-thread connection-handlers))))
 
 (defun stop-server (server)
   (declare (type server server))
-  (with-slots (tcp-server) server
+  (with-slots (tcp-server connection-handlers) server
+    (mapc 'system:join-thread connection-handlers)
+    (setf connection-handlers nil)
     (when tcp-server
       (system:stop-tcp-server tcp-server)
       (setf tcp-server nil)
