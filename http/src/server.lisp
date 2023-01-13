267d4182e3626f3fe41c1298bd86196289a0366e
Nicolas Martyanoff <nicolas@n16f.net>
2023-01-13 16:04:58 +0100

    http: handle connections

--- http/src/server.lisp
+++ http/src/server.lisp
@@ -5,42 +5,106 @@
     :type system:mutex
     :initform (system:make-mutex :name "http-server")
     :reader server-mutex)
+   (closingp
+    :type boolean
+    :initform nil)
    (tcp-server
     :type (or system:tcp-server null)
-    :initform nil
-    :accessor server-tcp-server)
+    :initform nil)
+   (connections
+    :type list
+    :initform nil)
+   (connections-condition-variable
+    :type system:condition-variable
+    :initform (system:make-condition-variable :name "http-server-connections"))
    (connection-handlers
     :type list
-    :initform nil
-    :accessor server-connection-handlers)))
+    :initform nil)))
+
+(defclass connection ()
+  ((stream
+    :type system:network-stream
+    :initarg :stream
+    :reader connection-stream)))
 
 (defun start-server (host port &key (nb-threads 1))
   (let ((server (make-instance 'server))
         (connection-handlers nil))
     (core:abort-protect
-        (progn
+        (flet ((handle-connection (stream)
+                 (let ((connection (make-instance 'connection :stream stream)))
+                   (server-handle-connection server connection))))
           (dotimes (i nb-threads)
             (push (system:make-thread "http-connection-handler"
                                       (lambda ()
-                                        ;; TODO
-                                        nil))
+                                        (connection-handler server)))
                   connection-handlers))
-          (setf (server-connection-handlers server) connection-handlers)
-          (setf (server-tcp-server server)
-                (system:start-tcp-server
-                 host port
-                 (lambda (stream)
-                   ;; TODO
-                   (close stream))))
+          (setf (slot-value server 'connection-handlers) connection-handlers)
+          (setf (slot-value server 'tcp-server)
+                (system:start-tcp-server host port #'handle-connection))
           server)
       (mapc 'system:join-thread connection-handlers))))
 
 (defun stop-server (server)
   (declare (type server server))
-  (with-slots (tcp-server connection-handlers) server
+  (with-slots (mutex closingp tcp-server connection-handlers connections)
+      server
+    (system:with-mutex (mutex)
+      (setf closingp t))
     (mapc 'system:join-thread connection-handlers)
     (setf connection-handlers nil)
+    (mapc 'close-connection connections)
     (when tcp-server
       (system:stop-tcp-server tcp-server)
       (setf tcp-server nil)
       t)))
+
+(defun server-handle-connection (server connection)
+  (declare (type server server)
+           (type connection connection))
+  (with-slots (stream) connection
+    (core:abort-protect
+        (progn
+          (setf (system:io-stream-external-format stream)
+                '(:ascii :eol-style :crlf))
+          (setf (system:network-stream-read-timeout stream) 30000
+                (system:network-stream-write-timeout stream) 30000)
+          (server-enqueue-connection server connection))
+      (close-connection connection))))
+
+(defun server-enqueue-connection (server connection)
+  (declare (type server server)
+           (type connection connection))
+  (with-slots (mutex connections connections-condition-variable) server
+    (system:with-mutex (mutex)
+      (push connection connections)
+      (when (null (cdr connections))
+        (system:signal-condition-variable connections-condition-variable)))))
+
+(defun server-pop-connection (server)
+  (declare (type server server))
+  (with-slots (mutex connections connections-condition-variable) server
+    (system:with-mutex (mutex)
+      (system:wait-condition-variable connections-condition-variable mutex
+                                      :timeout 1.0)
+      (pop connections))))
+
+(defun connection-handler (server)
+  (do ((mutex (server-mutex server)))
+      ((system:with-mutex (mutex) (slot-value server 'closingp))
+       nil)
+    (let ((connection (server-pop-connection server)))
+      (when connection
+        (core:abort-protect
+            (handle-connection server connection)
+          (close-connection connection))))))
+
+(defun handle-connection (server connection)
+  ;; TODO
+  (sleep 0.25)
+  (server-enqueue-connection server connection))
+
+(defun close-connection (connection)
+  (declare (type connection connection))
+  (with-slots (stream) connection
+    (ignore-errors (close stream))))
