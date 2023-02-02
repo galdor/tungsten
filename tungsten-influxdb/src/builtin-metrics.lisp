6e70dcfa50c864adde38087e2b4f5efa6ddcce47
Nicolas Martyanoff <nicolas@n16f.net>
2023-02-02 18:09:56 +0100

    influxdb: collect file descriptor count and limit

--- tungsten-influxdb/src/builtin-metrics.lisp
+++ tungsten-influxdb/src/builtin-metrics.lisp
@@ -4,9 +4,17 @@
   "The time between collections of builtin metrics in seconds.")
 
 (defun collect-builtin-metrics ()
-  (append (collect-builtin-metrics/threads)))
+  (append (collect-builtin-metrics/threads)
+          (collect-builtin-metrics/file-descriptors)))
 
 (defun collect-builtin-metrics/threads ()
   (let ((nb-threads (length (system:list-threads))))
     (list (make-point "tungsten.lisp.nb_threads"
                       :fields `(("count" . ,nb-threads))))))
+
+(defun collect-builtin-metrics/file-descriptors ()
+  (let ((count (system:count-file-descriptors))
+        (limit (system:file-descriptor-limit)))
+    (list (make-point "tungsten.system.file_descriptors"
+                      :fields `(("count" . ,count)
+                                ("limit" . ,limit))))))
