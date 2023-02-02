66a4c2a132a5a0940245432e353d251dc8541a4b
Nicolas Martyanoff <nicolas@n16f.net>
2023-02-02 19:38:19 +0100

    influxdb: collect memory usage metrics

--- tungsten-influxdb/src/builtin-metrics.lisp
+++ tungsten-influxdb/src/builtin-metrics.lisp
@@ -5,12 +5,21 @@
 
 (defun collect-builtin-metrics ()
   (append (collect-builtin-metrics/threads)
+          (collect-builtin-metrics/memory)
           (collect-builtin-metrics/file-descriptors)))
 
 (defun collect-builtin-metrics/threads ()
   (let ((nb-threads (length (system:list-threads))))
     (list (make-point "tungsten.lisp.nb_threads"
                       :fields `(("count" . ,nb-threads))))))
+
+(defun collect-builtin-metrics/memory ()
+  (multiple-value-bind (virtual resident shared)
+      (system:memory-usage)
+    (list (make-point "tungsten.system.memory"
+                      :fields `(("virtual" . ,virtual)
+                                ("resident" . ,resident)
+                                ("shared" . ,shared))))))
 
 (defun collect-builtin-metrics/file-descriptors ()
   (let ((count (system:count-file-descriptors))
