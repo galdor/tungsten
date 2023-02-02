b2483732a51f96285cef23d2befed753cfd8f056
Nicolas Martyanoff <nicolas@n16f.net>
2023-02-02 12:44:11 +0100

    influxdb: simplify the printed representation of points

--- tungsten-influxdb/src/influxdb.lisp
+++ tungsten-influxdb/src/influxdb.lisp
@@ -23,10 +23,8 @@
 
 (defmethod print-object ((point point) stream)
   (print-unreadable-object (point stream :type t)
-    (with-slots (measurement timestamp) point
-      (format stream "~S ~D"
-              measurement
-              (time:datetime-unix-timestamp timestamp :unit :nanosecond)))))
+    (with-slots (measurement) point
+      (prin1 measurement stream))))
 
 (defun make-point (measurement
                    &key tags fields (timestamp (time:current-datetime)))
