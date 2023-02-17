5f10373ca54f9b37c047571cc43aa8fd2d250e69
Nicolas Martyanoff <nicolas@n16f.net>
2023-02-17 16:01:07 +0100

    mime: add a json mapping for media ranges

--- tungsten-mime/src/json.lisp
+++ tungsten-mime/src/json.lisp
@@ -1,6 +1,7 @@
 (in-package :mime)
 
 (json:register-mapping-class 'media-type 'media-type-json-mapping)
+(json:register-mapping-class 'media-range 'media-range-json-mapping)
 
 (defclass media-type-json-mapping (json:mapping)
   ()
@@ -18,3 +19,21 @@
 (defmethod json:generate-value (value (mapping media-type-json-mapping))
   (declare (ignore mapping))
   (serialize-media-type value))
+
+(defclass media-range-json-mapping (json:mapping)
+  ()
+  (:default-initargs
+   :base-types '(:string)))
+
+(defmethod json:validate-value (value (mapping media-range-json-mapping))
+  (handler-case
+      (parse-media-range value)
+    (invalid-media-range (condition)
+      (json:add-mapping-error value
+                              "string is not a valid MIME media range: ~A"
+                              (invalid-media-range-description condition))
+      value)))
+
+(defmethod json:generate-value (value (mapping media-range-json-mapping))
+  (declare (ignore mapping))
+  (serialize-media-range value))
