0bd14bbdabceab3d4ad2fda34c14612b0c66312a
Nicolas Martyanoff <nicolas@n16f.net>
2023-08-05 13:10:22 +0200

    libssh: introduce a session class

--- tungsten-libssh/src/sessions.lisp
+++ tungsten-libssh/src/sessions.lisp
@@ -1,5 +1,11 @@
 (in-package :libssh)
 
+(defclass session ()
+  ((%session
+    :type ffi:pointer
+    :initarg :%session
+    :reader session-%session)))
+
 (defun connect-session (host &key (port 22))
   (declare (type system:host host)
            (type system:port-number port))
@@ -11,17 +17,18 @@
           (ssh-options-set/string %session :ssh-options-host host)
           (ssh-options-set/int %session :ssh-options-fd socket)
           (ssh-connect %session)
-          %session)
+          (make-instance 'session :%session %session))
       (when %session
         (ssh-free %session))
       (when socket
         (system:close-fd socket)))))
 
-(defun disconnect-session (%session)
-  (declare (type ffi:pointer %session))
-  (unwind-protect
-       (ssh-disconnect %session)
-    (ssh-free %session)))
+(defun disconnect-session (session)
+  (declare (type session session))
+  (with-slots (%session) session
+    (unwind-protect
+         (ssh-disconnect %session)
+      (ssh-free %session))))
 
 (defmacro with-session ((session &rest args) &body body)
   `(let ((,session (connect-session ,@args)))
